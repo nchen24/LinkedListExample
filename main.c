81247e0cd1902c0c693f739942f21ec9b0ca79b0
Nick Chen <chen_nicholas@cyberagent.co.jp>
2015-04-20 13:02:49 +0900

    Adds test files

--- main.c
+++ main.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <stdbool.h>
 #include "linkedlist.h"
 
 #define getArrLen(arr) (sizeof(arr) / sizeof(arr[0]))
+#define DELETE_DEFINED 1
+#define GETLENGTH_DEFINED 1
+
+typedef struct DArray{
+    int size;
+    int *contents;
+} DArray;
+
+Node *makeRandListOfSizeN(int n){
+    Node *list = NULL;
+    for(int i = 0 ; i < n ; i++){
+        list = addToHead(list, arc4random());
+    }
+    return list;
+}
+
+DArray arrayify(Node *list){
+    DArray arr;
+    arr.size = getNumElements(list);
+    arr.contents = malloc(sizeof(int) * arr.size);
+    Node *iter = list;
+    for(int i = 0 ; i < arr.size ; i++){
+        arr.contents[i] = iter->val;
+        iter = iter->next;
+    }
+    return arr;
+}
 
 void testMakeList(){
     int testCases[] = {8, 'n', -1, 149327859816738, -583492752983568732};
 
     for(int i = 0 ; i < getArrLen(testCases) ; i++){
-        Node *test;
-        test = makeList(testCases[i]);
-        printList(test);
+        Node *list;
+        list = makeList(testCases[i]);
+        printList(list);
+#if DELETE_DEFINED
+        delList(list);
+#endif
     }
 
     printf(">> Test make list completed! <<\n");
@@ -19,22 +50,28 @@ void testMakeList(){
 }
 
 void testAddToHead(){
-    Node *test = NULL;
-    test = addToHead(test, 5);
-    printList(test);
-    test = addToHead(test, 19);
-    printList(test);
+    Node *list = NULL;
+    list = addToHead(list, 5);
+    printList(list);
+    list = addToHead(list, 19);
+    printList(list);
+#if DELETE_DEFINED
+        delList(list);
+#endif
     printf(">> Test add to head completed! <<\n");
 }
 
 void testGetNumElements(){
     int vals[] = {0, 3, -153, 33895, 3049, 101010};
-    Node *test = NULL;
-    assert(getNumElements(test) == 0);
+    Node *list= NULL;
+    assert(getNumElements(list) == 0);
     for(int i = 0 ; i < getArrLen(vals) ; i++){
-        test = addToHead(test, vals[i]);
-        assert(getNumElements(test) == i+1);
+        list = addToHead(list, vals[i]);
+        assert(getNumElements(list) == i+1);
     }
+#if DELETE_DEFINED
+        delList(list);
+#endif
     printf(">> Test get num elements completed! <<\n");
 }
 
@@ -57,15 +94,34 @@ void testFindElement(){
         assert((temp != NULL) == answers[i]);
         printf("Test %d in testFindElement passed!\n", i);
     }
+#if DELETE_DEFINED
+        delList(list);
+#endif
 
     printf(">> Test find element completed! <<\n");
 }
 
+void testDelList(){
+    Node *list = NULL;
+    delList(list);
+
+    list = makeRandListOfSizeN(10);
+    //printList(list);
+    delList(list);
+
+    list = makeRandListOfSizeN(100);
+    //printList(list);
+    delList(list);
+    printf(">> Test del list completed! <<\n");
+}
+
 int main(){
-    testMakeList();
+    srand(time(0));
+    testMakeList(); // Also tests print
     testAddToHead();
     testGetNumElements();
     testFindElement();
+    testDelList();
 
     return 0;
 }
