e73f0a71429908a28bd24a93aed2cf8ac5b49cf5
Nick Chen <chen_nicholas@cyberagent.co.jp>
2015-04-20 12:00:45 +0900

    Fixes changeValueAtN, adds Makefile

--- linkedlist.h
+++ linkedlist.h
@@ -3,19 +3,19 @@
 
 typedef struct Node{
     int val;
-    Node *next;
+    struct Node *next;
 } Node;
 
-Node *makeList(int v);                  // #1
-Node *addToHead(Node *l, int v);        // #3
-Node *addToTail(Node *l, int v);        // #7
-Node *addToAtN(Node *l, int v, int n);  // #9
-Node *deleteAtN(Node *l, int n);        // #10
-Node *changeValueAtN(Node *l, int n);   // #8
-int getNumElements(Node *l);            // #4
-Node *findElement(Node *l, int v);      // #5
-void delList(Node *l);                  // #6
-void printList(Node *l);                // #2
+Node *makeList(int v);                          // #1
+Node *addToHead(Node *l, int v);                // #3
+Node *addToTail(Node *l, int v);                // #7
+Node *addToAtN(Node *l, int v, int n);          // #9
+Node *deleteAtN(Node *l, int n);                // #10
+Node *changeValueAtN(Node *l, int v, int n);    // #8
+int getNumElements(Node *l);                    // #4
+Node *findElement(Node *l, int v);              // #5
+void delList(Node *l);                          // #6
+void printList(Node *l);                        // #2
 
 
 static Node* traverseToN(Node *l, int n);
